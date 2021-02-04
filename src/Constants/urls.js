6d216eaa2925a2767e935c4978942ab6211e3a5a
Abhishek Meruga <abhishek_meruga@epam.com>
2021-02-04 20:23:59 +0530

    initial commit

---
+++ src/Constants/urls.js
@@ -0,0 +1,6 @@
+const urls = {
+    specificZone: 'http://api.timezonedb.com/v2/get-time-zone?key=XWSLLPX5RMIZ&format=json&by=zone&zone=',
+    zones:'http://api.timezonedb.com/v2.1/list-time-zone?key=XWSLLPX5RMIZ&format=json&zone=Europe/*'    
+}
+
+module.exports = urls;
---
+++ src/components/timezone.component.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import urls from '../Constants/urls'
+
+export class Timezone extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            zones : [],
+            selectedValue: '',
+            selectedTime: '',
+            loading: false
+        }
+        this.selectTimeZone = this.selectTimeZone.bind(this);
+        this.callSelectedTime = this.callSelectedTime.bind(this)
+    }
+
+    clearInterval = '';
+
+    selectTimeZone (event) {
+        this.setState({
+            selectedValue : event.target.value
+        }, ()=>{
+            this.callSelectedTime(this.state.selectedValue)
+        })
+    }
+
+    async callSelectedTime (zone) {
+        const selectedZone = await axios.get(`${urls.specificZone}${zone}`);
+        this.setState({
+            selectedTime: selectedZone.data.formatted
+        })
+    }
+    
+    async componentDidMount() {
+        const data = await axios.get(`${urls.zones}`)
+        this.setState({
+            zones : data.data.zones,
+            loading: true
+        },()=>{
+            this.setState({
+                selectedValue: this.state.zones[0].zoneName
+            })
+        })
+        this.clearInterval = setInterval(()=>{
+            this.callSelectedTime(this.state.selectedValue)
+        },5000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.clearInterval);
+    }
+
+    // - {(new Date()).toLocaleString('en-GB', { timeZone: zone.zoneName })}
+    render() {
+        return (<>
+        {!this.state.loading?<h2>Loading Time Zones...</h2>:
+            <div>
+                <select style={{width:'200px'}} onChange={this.selectTimeZone} value={this.state.selectedValue}>
+                    {
+                        this.state.zones.map((zone,index) => <option key={index} >{zone.zoneName}</option>)
+                    }
+                </select><br/>
+                <h2>Selected Zone and Current time</h2>
+                <div><b>Zone : </b>{this.state.selectedValue}</div> <b>Time : </b>{this.state.selectedTime}
+
+                <h5><u>Note:</u> Selected zone time will be updated every 5'sec</h5>
+            </div>
+        }
+            </>
+        )
+    }
+}
+
+export default Timezone
